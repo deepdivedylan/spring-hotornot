484dda82cc7e82fcc7a64777ef8e938dd4380d68
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 17:48:21 -0600

    added mySQL schema

---
+++ sql/spring-hotornot.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS rating;
+DROP TABLE IF EXISTS captain;
+
+CREATE TABLE captain (
+  captainId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  captainName VARCHAR(32) NOT NULL,
+  captainImage VARCHAR(128) NOT NULL,
+  PRIMARY KEY(captainId)
+);
+
+CREATE TABLE rating (
+  ratingId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  ratingCaptainId INT UNSIGNED NOT NULL,
+  ratingTeaEarlGreyHot TINYINT UNSIGNED NOT NULL,
+  FOREIGN KEY(ratingCaptainId) REFERENCES captain(captainId),
+  PRIMARY KEY(ratingCaptainId)
+);
