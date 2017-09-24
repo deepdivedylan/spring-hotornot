517764acc5c17dc68f2c9f209c5ab181a760b863
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 18:15:39 -0600

    created captain DTO

--- src/main/java/io/deepdivedylan/springhotornot/rest/dto/Captain.java
+++ src/main/java/io/deepdivedylan/springhotornot/rest/dto/Captain.java
@@ -1,4 +1,31 @@
 package io.deepdivedylan.springhotornot.rest.dto;
 
 public class Captain {
+    private Long captainId;
+    private String captainName;
+    private String captainImage;
+
+    public Long getCaptainId() {
+        return captainId;
+    }
+
+    public void setCaptainId(Long captainId) {
+        this.captainId = captainId;
+    }
+
+    public String getCaptainName() {
+        return captainName;
+    }
+
+    public void setCaptainName(String captainName) {
+        this.captainName = captainName;
+    }
+
+    public String getCaptainImage() {
+        return captainImage;
+    }
+
+    public void setCaptainImage(String captainImage) {
+        this.captainImage = captainImage;
+    }
 }
