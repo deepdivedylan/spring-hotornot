630339c77dc94bb6e8f2f9680d8b9f0f8f82eba9
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 18:33:11 -0600

    added concrete DAO

--- src/main/java/io/deepdivedylan/springhotornot/dao/CaptainDAORepository.java
+++ src/main/java/io/deepdivedylan/springhotornot/dao/CaptainDAORepository.java
@@ -1,5 +1,32 @@
 package io.deepdivedylan.springhotornot.dao;
 
+import io.deepdivedylan.springhotornot.entities.Captain;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("captainDAO")
+@Transactional
 public class CaptainDAORepository implements CaptainDAO {
 
+    @PersistenceContext
+    public EntityManager admirality;
+
+    @Transactional(readOnly = true)
+    public Captain getCaptain(Long captainId) {
+        try {
+            String query = "SELECT captainId, captainImage, captainName FROM captain WHERE captainId = " + captainId;
+            return((Captain)admirality.createQuery(query).getSingleResult());
+        } catch(Exception exception) {
+            return(null);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Captain> getAllCaptains() {
+        return(admirality.createQuery("SELECT captainId, captainImage, captainName FROM captain").getResultList());
+    }
 }
