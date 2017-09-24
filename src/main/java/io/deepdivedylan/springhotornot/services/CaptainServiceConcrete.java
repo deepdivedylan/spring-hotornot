6705f0e451ac1a6cc6a99550b48c62489b927804
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 18:47:16 -0600

    added captain service

--- src/main/java/io/deepdivedylan/springhotornot/services/CaptainServiceConcrete.java
+++ src/main/java/io/deepdivedylan/springhotornot/services/CaptainServiceConcrete.java
@@ -1,4 +1,41 @@
 package io.deepdivedylan.springhotornot.services;
 
-public class CaptainServiceConcrete {
+import io.deepdivedylan.springhotornot.dao.CaptainDAO;
+import io.deepdivedylan.springhotornot.entities.Captain;
+import io.deepdivedylan.springhotornot.rest.dto.CaptainDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service("captainService")
+@Transactional
+public class CaptainServiceConcrete implements CaptainService {
+
+    @Autowired(required = true)
+    private CaptainDAO captainDAO;
+
+    public CaptainDTO getCaptain(Long captainId) {
+        Captain captain = captainDAO.getCaptain(captainId);
+        CaptainDTO captainDTO = new CaptainDTO();
+        captainDTO.setCaptainId(captain.getCaptainId());
+        captainDTO.setCaptainImage(captain.getCaptainImage());
+        captainDTO.setCaptainName(captain.getCaptainName());
+        return(captainDTO);
+    }
+
+    public List<CaptainDTO> getAllCaptains() {
+        List<Captain> results = captainDAO.getAllCaptains();
+        List<CaptainDTO> captainDTOs = new LinkedList<CaptainDTO>();
+        for(Captain captain: results) {
+            CaptainDTO captainDTO = new CaptainDTO();
+            captainDTO.setCaptainId(captain.getCaptainId());
+            captainDTO.setCaptainImage(captain.getCaptainImage());
+            captainDTO.setCaptainName(captain.getCaptainName());
+            captainDTOs.add(captainDTO);
+        }
+        return(captainDTOs);
+    }
 }
