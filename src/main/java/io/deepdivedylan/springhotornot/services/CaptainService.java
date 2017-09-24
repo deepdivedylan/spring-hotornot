494b1f6774d1e7ef8674cf3f1e0a33bbce4d3d76
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 18:39:11 -0600

    added captain service interface

--- src/main/java/io/deepdivedylan/springhotornot/services/CaptainService.java
+++ src/main/java/io/deepdivedylan/springhotornot/services/CaptainService.java
@@ -1,4 +1,10 @@
 package io.deepdivedylan.springhotornot.services;
 
+import io.deepdivedylan.springhotornot.rest.dto.CaptainDTO;
+
+import java.util.List;
+
 public interface CaptainService {
+    public CaptainDTO getCaptain(Long captainId);
+    public List<CaptainDTO> getAllCaptains();
 }
