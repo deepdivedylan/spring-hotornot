93a7221287dcd2fb6ec0b1dc6fc9721e95966156
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-23 18:19:25 -0600

    created captain entity

--- src/main/java/io/deepdivedylan/springhotornot/entities/Captain.java
+++ src/main/java/io/deepdivedylan/springhotornot/entities/Captain.java
@@ -1,4 +1,37 @@
 package io.deepdivedylan.springhotornot.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "captain")
 public class Captain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long captainId;
+    private String captainName;
+    private String captainImage;
+
+    public Long getCaptainId() {
+        return captainId;
+    }
+
+    public void setCaptainId(Long captainId) {
+        this.captainId = captainId;
+    }
+
+    public String getCaptainName() {
+        return captainName;
+    }
+
+    public void setCaptainName(String captainName) {
+        this.captainName = captainName;
+    }
+
+    public String getCaptainImage() {
+        return captainImage;
+    }
+
+    public void setCaptainImage(String captainImage) {
+        this.captainImage = captainImage;
+    }
 }
